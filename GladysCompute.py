3fca4c75b38d3a4b8572574a8dce14e340c46303
M1nhTre37 <hugebeez@gmail.com>
2021-07-15 12:04:03 -0700

    added the distance formula

--- GladysCompute.py
+++ GladysCompute.py
@@ -10,4 +10,33 @@ import gladysSatellite as satellite
 # start of code by Minh Le
 
 def gpsAverage(x, y):
+    """ 
+    This function computes the GPS value average of a position by taking a GPS value and dividing by 2 
     """
+    
+    #Get coordinates from data returned by gpsValue
+    latitude = satellite.gpsValue(x, y, "latitude")[0][0]
+    longtitude = satellite.gpsValue(x, y, "longtitude")[0][0]
+    altitude = satellite.gpsValue(x, y, "altitude")[0][0]
+    time = satellite.gpsValue(x, y, "time")[0][0]
+    
+    #Compute average
+    average = (latitude + longtitude + altitude + time) / 4
+
+    return average
+#End of code by Minh Le
+
+
+
+
+#Start of code by Hubert Pham
+def distance(current, dest):
+	import math
+	#initialize variables
+	aveg1 = 0
+	aveg2 = 0
+	distance = 0
+	aveg1 = math.pow(gpsAverage(current[0], current[1]), 2)
+	aveg2 = math.pow(gpsAverage(dest[0], dest[1], ), 2)
+	distance = math.sqrt(aveg1 + aveg2 )
+	return distance
